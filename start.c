6152728fd72eb8c905fa0bd0b4e76d3b4e202435
Sushant Dhakal -----(motas) <sushantdhakal98@gmail.com>
2026-02-23 12:36:02 +0545

    Add files via upload

---
+++ start.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    int sum;
+
+    printf("enter any number u want:");
+    scanf("%d",&a);
+    printf("enter any number u want:");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("the sum of given number is %d",sum);
+}
